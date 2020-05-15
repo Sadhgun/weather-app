18e9c7851306777151a774e0a3082e0e46ccd47b
Sadhgun <sadhgunsobti@gmail.com>
2020-05-15 14:36:32 +0530

    Updates

---
+++ precache-manifest.9346a81670de0f54f7a8af30f5df8645.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ea09bf0a454ac0b6265e2a36d59379b",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "3f14aad5a56a18ecdecb",
+    "url": "/weather-app/static/css/main.a511d133.chunk.css"
+  },
+  {
+    "revision": "119f8b326d08107942e3",
+    "url": "/weather-app/static/js/2.c831ed82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.c831ed82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f14aad5a56a18ecdecb",
+    "url": "/weather-app/static/js/main.6d76ed15.chunk.js"
+  },
+  {
+    "revision": "b583aba1370634b5a5a2",
+    "url": "/weather-app/static/js/runtime-main.60cbc992.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/weather-app/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/weather-app/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
